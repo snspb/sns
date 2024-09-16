d5b5b211d89bf736cb58902d090b597507660c46
Shubham Naagar <sksn760@gmail.com>
2024-09-16 08:30:28 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -233,4 +233,36 @@ AOS.init({
     fixedContentPos: false,
   });
 })(jQuery);
-console;
+
+/*-------------Typing Animation------------------ */
+const typingAnimationElement = document.getElementById("typing-animation");
+
+// Create an array of typing text
+const typingTexts = [
+  "Web Developer     ",
+  "Graphic Designer    ",
+  "Software Developer     ",
+
+];
+
+// Create a function to display the typing animation for a given text
+function playTypingAnimation(text) {
+  // Loop through each character and add it to the element
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingAnimationElement.textContent += text[i];
+    }, i * 100); // Decreased delay to speed up the typing animation
+  }
+
+  // Once the animation is complete, reset the text and start over
+  setTimeout(() => {
+    typingAnimationElement.textContent = "";
+    playTypingAnimation(
+      typingTexts[(typingTexts.indexOf(text) + 1) % typingTexts.length]
+    );
+  }, text.length * 100); // Adjusted delay accordingly
+}
+
+// Start the typing animation loop
+playTypingAnimation(typingTexts[0]);
+
